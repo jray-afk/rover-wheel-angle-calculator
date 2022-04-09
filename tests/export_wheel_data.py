8eee013702c7a1721901ee8c2dc901ea78f6de14
flyingray <flyingray>
2022-04-09 16:24:03 -0700

    pep formatting

--- tests/export_wheel_data.py
+++ tests/export_wheel_data.py
@@ -3,7 +3,6 @@ sys.path.append('src')
 from rover_wheel_angle_calculator import RoverWheelAngleCalculator
 
 
-
 ROVER_Y = 10  # from center y length of rover to front wheels; should be the same distance as center to back
 ROVER_X = 7  # full x width of rover
 wheel_angle_calculator = RoverWheelAngleCalculator(
